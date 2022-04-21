511e31919f4d2cc11ba8b340f63a579076cdb14e
Sam Sharff <samanthaleec@gmail.com>
2022-04-21 11:34:41 -0400

    attempts to add Handsontable

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,32 @@
+<script>
+import { createApp } from "vue";
+import { HotTable } from "@handsontable/vue3";
+import { registerAllModules } from "handsontable/registry";
+import { createSpreadsheetData } from "./helpers";
+
+// register Handsontable's modules
+registerAllModules();
+
+const app = createApp({
+  data() {
+    return {
+      hotSettings: {
+        data: createSpreadsheetData(6, 10),
+        colHeaders: true,
+        height: "auto",
+        licenseKey: "non-commercial-and-evaluation",
+      },
+    };
+  },
+  components: {
+    HotTable,
+  },
+});
+app.mount("#example1");
+</script>
+
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div id="example1">
+    <hot-table :settings="hotSettings"></hot-table>
   </div>
 </template>
-
-<script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
-export default {
-  name: "HomeView",
-  components: {
-    HelloWorld,
-  },
-};
-</script>
